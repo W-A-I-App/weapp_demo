3a265dbe41b8bd7f070afcc23745cd79fc3f1fbd
mount-brother <mount_brother@163.com>
2017-09-22 00:13:20 +0800

    Add static "Mine" Page
    Add static "Mine" Page

--- client/pages/Mine/mine.js
+++ client/pages/Mine/mine.js
@@ -1,66 +1,41 @@
-// pages/Mine/mine.js
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    userInfo: {},
+    userListInfo: [{
+      icon: '../../images/Mine/wddd.png',
+      text: '我的订单',
+      isunread: true,
+      unreadNum: 2
+    }, {
+      icon: '../../images/Mine/wddjq.png',
+      text: '我的代金券',
+      isunread: false,
+      unreadNum: 2
+    }, {
+      icon: '../../images/Mine/gwc.png',
+      text: '购物车',
+      isunread: true,
+      unreadNum: 1
+    }, {
+      icon: '../../images/Mine/shdzgl.png',
+      text: '收货地址管理'
+    }, {
+      icon: '../../images/Mine/lxkf.png',
+      text: '联系客服'
+    }, {
+      icon: '../../images/Mine/cjwt.png',
+      text: '常见问题'
+    }]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  onLoad: function () {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    })
   }
 })
