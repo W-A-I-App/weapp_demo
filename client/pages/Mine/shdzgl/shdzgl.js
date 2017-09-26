1d30573aa002aff0853d9f4433b3bd23c0254793
mount-brother <mount_brother@163.com>
2017-09-26 23:55:56 +0800

    Add sql insert function
    Add sql insert function(not succeed yes)

--- client/pages/Mine/shdzgl/shdzgl.js
+++ client/pages/Mine/shdzgl/shdzgl.js
@@ -3,9 +3,32 @@ Page({
     logs: []
   },
   onLoad: function() {
-   this.getdata();
+   //this.getdata();
+    this.postdata();
   },
-  getdata: function() {//定义函数名称
+  postdata: function() {//定义函数名称
+    var that = this;   // 这个地方非常重要，重置data{}里数据时候setData方法的this应为以及函数的this, 如果在下方的sucess直接写this就变成了wx.request()的this了
+    wx.request({
+      url: 'https://gcdojwey.qcloud.la/weapp/usr_address_post',//请求地址
+      data: {//发送给后台的数据
+        id: 4,
+        usr_id: "chenglei04",
+        usr_name: "renchenglei4",
+        usr_adress: "shanghaiminhang",
+        first_choice: 0
+      },
+      header: {//请求头
+        "Content-Type": "applciation/json"
+      },
+      method: "POST",//get为默认方法/POST
+      success: function (res) {
+        console.log(res);//res.data相当于ajax里面的data,为后台返回的数据
+      },
+      fail: function (err) { },//请求失败
+      complete: function () { }//请求完成后执行的函数
+    })
+  },
+  getdata: function () {//定义函数名称
     var that = this;   // 这个地方非常重要，重置data{}里数据时候setData方法的this应为以及函数的this, 如果在下方的sucess直接写this就变成了wx.request()的this了
     wx.request({
       url: 'https://gcdojwey.qcloud.la/weapp/usr_address',//请求地址
@@ -19,9 +42,9 @@ Page({
       success: function (res) {
         console.log(res.data);//res.data相当于ajax里面的data,为后台返回的数据
         console.log(res.data.data[0].usr_name)
-      　that.setData({//如果在sucess直接写this就变成了wx.request()的this了.必须为getdata函数的this,不然无法重置调用函数
+        that.setData({//如果在sucess直接写this就变成了wx.request()的this了.必须为getdata函数的this,不然无法重置调用函数
           logs: res.data.data[0].usr_adress
-　　　　 })
+        })
       },
       fail: function (err) { },//请求失败
       complete: function () { }//请求完成后执行的函数
