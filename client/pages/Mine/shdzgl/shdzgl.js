f474b9828219cfd9690dce5d8131eac48f527514
mount-brother <32101369+mount-brother@users.noreply.github.com>
2017-10-03 22:39:11 +0800

    Add delete function from client side
    Add delete function from client side

--- client/pages/Mine/shdzgl/shdzgl.js
+++ client/pages/Mine/shdzgl/shdzgl.js
@@ -5,7 +5,8 @@ Page({
   onLoad: function() {
     //this.select();
     //this.insert();
-    this.update();
+    //this.update();
+    this.delete();
   },
 
   insert: function() {//定义函数名称
@@ -61,6 +62,27 @@ Page({
         usr_id: "chenglei03",
         usr_adress: "shanghaiminhang",
         first_choice: 1
+      },
+      header: {//请求头
+        "Content-Type": "applciation/json"
+      },
+      method: "GET",//get为默认方法/POST
+      success: function (res) {
+        console.log(111);
+        console.log(res);//res.data相当于ajax里面的data,为后台返回的数据
+      },
+      fail: function (err) { },//请求失败
+      complete: function () { }//请求完成后执行的函数
+    })
+  },
+  
+  delete: function () {//定义函数名称
+    var that = this;   // 这个地方非常重要，重置data{}里数据时候setData方法的this应为以及函数的this, 如果在下方的sucess直接写this就变成了wx.request()的this了
+    wx.request({
+      url: 'https://gcdojwey.qcloud.la/weapp/usr_address_delete',//请求地址
+      data: {//发送给后台的数据
+        id: 3,
+        usr_id: "chenglei03"
       },
       header: {//请求头
         "Content-Type": "applciation/json"
