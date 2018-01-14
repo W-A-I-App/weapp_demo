11fd177b3cfec841023f05425c126b67c983c141
mount-brother <mount_brother@163.com>
2018-01-15 00:53:59 +0800

    Enhance tools and gaming pages
    Enhance tools and gaming pages, we will collect more useful tools and intesting games

---
+++ client/pages/Message/tools/cpy.js
@@ -0,0 +1,35 @@
+var pinyin = require('../../../lib/pinyin.js');
+Page({
+  data: {
+    pinyinText: '',
+  },
+  onLoad: function () {
+
+  },
+  // 事件处理函数
+  onInput: function (e) {
+    console.log(e)
+    var char = e.detail.value;
+    console.log('--', char);
+    char = char && char.trim();
+    if (char.length == 1) {
+      if (pinyin.hasOwnProperty(char)) {
+        console.log(pinyin[char].join(', '))
+        this.setData({
+          pinyinText: pinyin[char].join(', ')
+        });
+      }
+      else {
+        this.setData({
+          pinyinText: '找不到，^_^'
+        });
+      }
+    }
+    else {
+      this.setData({
+        pinyinText: ''
+      });
+    }
+  },
+
+})
