a9cb5842decf44a00a83af47558c92b404a73dae
mount-brother <mount_brother@163.com>
2018-03-04 00:15:56 +0800

    2048 game
    2048 game

---
+++ Magic/2048/config.js
@@ -0,0 +1,14 @@
+/**
+ * 小程序配置文件
+ */
+// 此处主机域名修改成腾讯云解决方案分配的域名
+var host = 'https://gcdojwey.qcloud.la';
+var config = {
+
+    // 下面的地址配合云端 Demo 工作
+    service: {
+        host
+    }
+};
+
+module.exports = config;
